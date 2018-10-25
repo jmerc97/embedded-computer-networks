424e55707d9799cb262c956af83b3231d297be6c
jmerc97 <joseph.d.mercer@gmail.com>
2018-10-25 14:43:15 +0100

    Changes to lab 2 task 2

--- lab_102/2_lcd_analog_display/src/main.c
+++ lab_102/2_lcd_analog_display/src/main.c
@@ -1,7 +1,7 @@
 /*
  * main.c
  *
- * this is the skeleton for task 2
+ * this is the main lcd application
  *
  * author:    Dr. Alex Shenfield
  * date:      04/09/2018
@@ -15,8 +15,24 @@
 #include "pinmappings.h"
 #include "clock.h"
 #include "stm32746g_discovery_lcd.h"
+#include "adc.h"
+#include "gpio.h"
+
+gpio_pin_t pinPot = {PA_0, GPIOA, GPIO_PIN_0};
+gpio_pin_t ledHalf = {PB_8, GPIOB, GPIO_PIN_8};
+gpio_pin_t ledFull = {PB_9, GPIOB, GPIO_PIN_9};
 
 // LCD DEFINES
+
+// define a message boarder (note the lcd is 28 characters wide using Font24)
+#define BOARDER     "****************************"
+
+// specify a welcome message
+const char * welcome_message[2] = 
+{
+  "*     Hello LCD World!     *",
+  "*      Welcome to SHU      *"
+};
 
 // CODE
 
@@ -27,5 +43,57 @@ int main()
   // properly
   HAL_Init();
   init_sysclk_216MHz();
+  init_adc(pinPot);
+	init_gpio(ledHalf, OUTPUT);
+	init_gpio(ledFull, OUTPUT);
+	
+  // initialise the lcd
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, SDRAM_DEVICE_ADDR);
+  BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
 
+  // set the background colour to blue and clear the lcd
+  BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+  BSP_LCD_Clear(LCD_COLOR_BLACK);
+  
+  // set the font to use
+  BSP_LCD_SetFont(&Font24); 
+  
+  // print the welcome message ...
+  BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
+  BSP_LCD_DisplayStringAtLine(0, (uint8_t *)BOARDER);
+  BSP_LCD_DisplayStringAtLine(1, (uint8_t *)welcome_message[0]);
+  BSP_LCD_DisplayStringAtLine(2, (uint8_t *)welcome_message[1]);
+  BSP_LCD_DisplayStringAtLine(3, (uint8_t *)BOARDER); 
+    
+  // delay a little ...
+  
+  while(1)
+  {
+    
+		uint16_t adc_val = read_adc(pinPot);
+		adc_val = adc_val / 4094.00 * 100 + 1;
+		
+		char str[12];
+		sprintf(str, "ADC = %4d", adc_val);
+		BSP_LCD_DisplayStringAtLine(6, (uint8_t *)str);
+		
+		if (adc_val < 50)
+		{
+			write_gpio(ledHalf, HIGH);
+			write_gpio(ledFull, LOW);
+		}
+		else if (adc_val > 50)
+		{
+			write_gpio(ledFull, HIGH);
+			write_gpio(ledHalf, LOW);
+		}
+		else if (adc_val == 50)
+		{
+			write_gpio(ledHalf, HIGH);
+			write_gpio(ledFull, HIGH);
+		}
+		
+		HAL_Delay(1000);
+  }
 }
